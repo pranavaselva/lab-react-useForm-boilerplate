508438f79e96af03bdff7f1639dd231c89342a2a
pranavaselva <pranava.m@kalvium.community>
2024-02-28 11:03:42 +0530

    completed

--- useForm-react/src/App.jsx
+++ useForm-react/src/App.jsx
@@ -1,90 +1,99 @@
-import { useState } from 'react'
-import {useForm} from 'react-hook-form'
-import './App.css'
+import  { useState } from "react";
+import {useForm} from "react-hook-form";
 
-function App() {
-  const [state, setState] = useState(false);
-  const {
+function App(){
+
+  const[state,setState] = useState(false)
+  const { 
     register,
-    handleSubmit,
-    formState:{errors},
-  } = useForm();
+    handleSubmit, 
+    formState: {errors}
+    } = useForm()
 
-  const SubmitHandler = (data) => {
-    setState(true);
-  };
+const SubmitForm = (data) => {
+setState(true)
+}
 
-  return (
-<div className='container'>
- <div className='container-2' >
-      <div className='register'>{state == true ? 'Registration Successful' : ''}</div>
-        <form onSubmit={handleSubmit(SubmitHandler)}>
-        <div>
-        <input type="text"
-        placeholder='First Name..'
-        {...register('firstName',{
-          required: 'please dont keep input feild empty',
-        })} />
+  return(
+    <div className="container">
+      <div className="main">
+      <div className="register">{state === true ? "Registration Successful" : null }</div>
 
-        <div style={{color:'red'}}>
-        {errors.firstName ? errors.firstName.message : null}
-      </div>
+<form onSubmit={handleSubmit(SubmitForm)}>
+
+<div>
+  <input type="text" placeholder="First Name"
+  {
+    ...register("firstName",{
+      required: "Please dont keep input field empty"
+    })
+  } />
+
+<div style={{color: "red"}}>
+  {errors.firstName ? errors.firstName.message : null}
+</div>
+</div>
+
+<div>
+  <input type="text" placeholder="Last Name"
+  {
+    ...register("lastName",{
+      required : "Please dont keep input field empty"
+    })
+  } />
+
+<div style={{color: "red"}}> 
+  {errors.lastName ? errors.lastName.message : null}
+</div>
+</div>
+
+<div>
+  <input type="email" placeholder="Email"
+  {
+    ...register("email",{
+      required: "Email is required",
+      validate: (value) =>{
+        if(!value.includes("@")){
+          return "Email must include @"
+        }
+        return true
+      }
+    })
+  }  />
+  <div style={{color:"red"}}>
+    {errors.email ? errors.email.message : null}
+
+  </div>
+
+</div>
+
+<div>
+  <input type="text" placeholder="Password"
+  {
+    ...register("password",{
+      required: true,
+      minLength : {
+        value : 5,
+        message : "Password should have atleast 5 characters"
+      },
+      maxLength : {
+        value : 20,
+        message : "Password cannot be more than 20 characters"
+      }
+    })
+  } />
+  <div>
+    {errors.password ? errors.password.message : null}
+  </div>
+</div>
+
+<input type="submit" value={"Register"} />
+
+</form>
+
+
     </div>
-    <div>
-      <input type="text"
-      placeholder='Last Name..'
-      {...register('lastName',{
-        required: 'Please dont keep input feild empty',
-      })} />
-        <div>
-         {errors.lastName ? errors.lastNmae.message : null}
-       </div>
     </div>
-
-    <div>
-      <input 
-        type="email"
-        placeholder='Email..'
-        { ...register('email',{
-          required:true,
-
-          validate: (value) => {
-            if(value.includes('@')){
-              return true;
-            }
-            return 'invalid email';
-          },
-        })        
-        } />
-        <div style={{color: 'red'}}>
-          {errors.email ? errors.email.message : null}
-        </div>
-    </div>
-    <div>
-      <input 
-        type="password" 
-        placeholder='Password..'
-        { ...register('password',{
-          required : true,
-          minLength :{
-            value:5,
-            message : 'Password must be more than 4 characters',
-          },
-          maxLength:{
-            value:20,
-            message:'Password cannot be more than 20 characters',
-          }
-        })
-        }/>
-        <div style={{color:'red'}}>
-      {errors.password ? errors.password.message:null}
-    </div>
-   </div>
-   <input className='register' type="submit" value={'Register'} />
-  </form>
- </div>
-</div>   
-
   )
 }
 
